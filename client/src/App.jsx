7b25cedcbfde349c4492fc8915e528c5dbefae3d
SRIDHARN <sridharganesh2881@gmail.com>
2024-04-30 22:52:29 +0530

    created routed all the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/home'
+import Dashboard from './pages/dashboard'
+import Signin from './pages/signin'
+import Signup from './pages/signup'
+import Project from './pages/project'
+import About from './pages/about'
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red'>sridharan</h1>
+    <BrowserRouter>
+    <Routes>
+       <Route path="/" element={<Home/>} />
+       <Route path="/sigin" element={<Signin />} />
+       <Route path="dashboard" element={<Dashboard />} />
+       <Route path="/signup" element={<Signup />} />
+       <Route path="/about" element={<About />} />
+       <Route path="/project" element={<Project />} />
+
+    </Routes>
+    
+    
+    </BrowserRouter>
   )
 }
+
