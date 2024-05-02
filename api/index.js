d8b799f8ca57817929d0fe9056a049f1e7b36496
SRIDHARN <sridharganesh2881@gmail.com>
2024-05-02 17:16:51 +0530

    Database connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
 const app=express()
+mongoose.connect("mongodb://localhost:27017/mernstack1").then(()=>{
+    console.log("Mongo db conneted");
+}).catch((e)=>{
+    console.log(e)
+})
 app.listen(3000,()=>{
     console.log('APP IS LISTENING TO THE PORT 3000!!')
     
